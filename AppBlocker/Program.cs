91fe16455a4c2004610794718c59c35d0f8b037f
Jeremy Browne <jeremy.browne@zerolatencyvr.com>
2020-09-23 06:58:14 +1000

    changes

--- AppBlocker/Program.cs
+++ AppBlocker/Program.cs
@@ -2,6 +2,9 @@
 using System.Diagnostics;
 using System.Management;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace AppBlocker
 {
@@ -13,11 +16,17 @@ namespace AppBlocker
             Process[] localAll = RemoveDuplicates(Process.GetProcesses());
             foreach (var process in localAll)
             {
-                Console.WriteLine(process.Id + " | " + process.ProcessName);
+                // Used for formatting
+                string pidString = process.Id.ToString();
+                string output = new string(Convert.ToChar(" "), 4 - pidString.Length);
+                Console.WriteLine(process.Id + output + " | " + process.ProcessName);
             }
+            string jsonString;
+            jsonString = JsonSerializer.Serialize<Process[]>(localAll);
+            File.WriteAllText(@"C:\\appBlocker\apps.json", jsonString);
         }
 
-        private static Process[] RemoveDuplicates(Process[] s)
+        static Process[] RemoveDuplicates(Process[] s)
         {
             HashSet<Process> set = new HashSet<Process>(s);
             Process[] result = new Process[set.Count];
@@ -25,7 +34,14 @@ namespace AppBlocker
             return result;
         }
 
-        private static void KillAllProcessesSpawnedBy(UInt32 parentProcessId)
+        public class AppRule
+        {
+            public string processName { get; set; }
+            /*public DateTime dtAllow { get; set; }
+            public DateTime dtDeny { get; set; }*/
+        }
+
+        static void KillAllProcessesSpawnedBy(UInt32 parentProcessId)
         {
             Console.WriteLine("Finding processes spawned by process with Id [" + parentProcessId + "]");
 
